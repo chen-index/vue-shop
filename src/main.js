acf3a36882d24d395bb0fe1b985c50b28c028907
IndexChen <949145820@qq.com>
2019-09-26 13:44:37 +0800

    is vueshop

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+import App from './App'
+import router from './router/index'
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    el:"#app",
+    render: h=>h(App),
+    router
+})
